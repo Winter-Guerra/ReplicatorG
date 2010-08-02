26129d50c126fdd4833be2e587d23ef4e746d900
xtremd <blackfishw95@gmail.com>
2010-08-02 18:02:24 -0400

    Removed the Z stage auto home buttons from the control panel.

--- src/replicatorg/app/ui/ControlPanelWindow.java
+++ src/replicatorg/app/ui/ControlPanelWindow.java
@@ -182,10 +182,8 @@ private JMenuItem makeAutoHomeItem(String name,final EnumSet<Axis> set,final boo
 		homeMenu.add(makeHomeItem("Home all-",EnumSet.allOf(Axis.class),false));
 		homeMenu.add(makeHomeItem("Home XZ-",EnumSet.of(Axis.X,Axis.Z),false));
 		homeMenu.add(makeHomeItem("Home YZ-",EnumSet.of(Axis.Y,Axis.Z),false));
-		homeMenu.add(makeFirstTimeAutoHomeItem("First Auto Home",EnumSet.of(Axis.Z),false)); //auto home downwards and save the distance
-		homeMenu.add(makeAutoHomeItem("Auto Home",EnumSet.of(Axis.Z),false)); //auto home downwards from the saved distance
-		homeMenu.add(makeFirstTimeAutoHomeItem("First Auto Home A",EnumSet.allOf(Axis.class),false)); //auto home downwards and save the distance
-		homeMenu.add(makeAutoHomeItem("Auto Home A",EnumSet.allOf(Axis.class),false)); //auto home downwards from the saved distance
+		homeMenu.add(makeFirstTimeAutoHomeItem("First Auto Home All",EnumSet.allOf(Axis.class),false)); //auto home downwards and save the distance
+		homeMenu.add(makeAutoHomeItem("Auto Home All",EnumSet.allOf(Axis.class),false)); //auto home downwards from the saved distance
 		return bar;
 	}
 
