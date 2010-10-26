a212300983310fb670c532e21fd9078e56aec9a3
xtremd <blackfishw95@gmail.com>
2010-10-26 11:06:12 -0400

    The commit button is now half wired up.

--- src/replicatorg/app/ui/HomingSetupWindow.java
+++ src/replicatorg/app/ui/HomingSetupWindow.java
@@ -53,6 +53,34 @@ public class HomingSetupWindow extends JFrame {
 	
 	
 	private void commit() {
+		// if negatives are selected then pass 1 else pass two, if Zaggo z-probe, pass Z 3.
+		byte direction[] = {0,0,0}; //array that holds the direction vals for XYZ. Zaggo Z-Probe is Z = 3.
+		
+if (EndstopPanel.xPlusButton.isSelected()) { //if xPlus button is selected then
+// set x packet to to 2
+	direction[0] = 2;
+	
+} else {
+	//set to 1 (forcing using at least 1 per axis)
+	direction[0] = 1;
+}
+if (EndstopPanel.yPlusButton.isSelected()) { //if xPlus button is selected then
+	// set x packet to to 2
+		direction[1] = 2;
+		
+	} else {
+		//set to 1 (forcing using at least 1 per axis)
+		direction[1] = 1;
+	}
+if (EndstopPanel.zPlusButton.isSelected()) { //if xPlus button is selected then
+	// set x packet to to 2
+		direction[2] = 2;
+		
+	} else {
+		//set to 1 (forcing using at least 1 per axis)
+		direction[2] = 1;
+	}
+		//set z mm
 		((OnboardParameters)driver).setZstageMMtoLift(zAxisMMToLift.getText());
 	}
 
@@ -74,6 +102,30 @@ public class HomingSetupWindow extends JFrame {
 			}
 		});
 		panel.add(cancelButton);
+		return panel;
+	}
+	
+	private JPanel makeConfigPanel() { //add the commit and cancel buttons to a panel
+		JPanel panel = new JPanel(new MigLayout());
+//label description of what this window is about
+		
+		JLabel description = new JLabel("<html>" + "For the Makerbot to home correctly, you must first select three endstops"
+				+ "<br>" + "that are currently installed on your 'bot (one for each axis). " + "</html>"); //OMG, do I really have to use HTML line breaks? 
+		panel.add(description, "wrap");//moving to next row here...
+		panel.add(createEndstopPanel(), "wrap");
+		
+		//ask if using Zaggo Z-Probe
+		panel.add(new JLabel("Next tell me, is Zaggo's Z-Probe hardware installed on this 'bot?"), "gaptop 25, wrap");
+		panel.add(ZaggoZprobe, "gapleft 30, wrap");
+		
+		//ask for zAxislift value (default 10);
+		zAxisMMToLift.setColumns(16);
+		zAxisMMToLift.setText(DefaultZAxisMMToLift);
+		panel.add(new JLabel("<html>"+"Finally, tell me how many millimeters should I lift the Z axis"
+				+"<br>"+"before attempting to home" + "<br>"
+				+"(To avoid accidental nozzle crashes into the build platform)."+"</html>"), "gaptop 25, wrap"); //Z axis lift
+		panel.add(zAxisMMToLift,"gapleft 30, wrap");
+		
 		return panel;
 	}
 	
@@ -101,25 +153,9 @@ public class HomingSetupWindow extends JFrame {
 		machine = m; //get our drivers
 		driver = machine.getDriver();
 		JPanel mainpanel = new JPanel(new MigLayout()); //start setting up the window
-		
-		//label description of what this window is about
-		
-		JLabel description = new JLabel("<html>" + "For the Makerbot to home correctly, you must first select three endstops"
-				+ "<br>" + "that are currently installed on your 'bot (one for each axis). " + "</html>"); //OMG, do I really have to use HTML line breaks? 
-		mainpanel.add(description, "wrap");//moving to next row here...
-		mainpanel.add(createEndstopPanel(), "wrap");
-		
-		//ask if using Zaggo Z-Probe
-		mainpanel.add(new JLabel("Next tell me, is Zaggo's Z-Probe hardware installed on this 'bot?"), "wrap");
-		mainpanel.add(ZaggoZprobe, "gapleft 30, wrap");
-		
-		//ask for zAxislift value (default 10);
-		zAxisMMToLift.setColumns(16);
-		zAxisMMToLift.setText(DefaultZAxisMMToLift);
-		mainpanel.add(new JLabel("<html>"+"Finally, tell me how many millimeters should I lift the Z axis"
-				+"<br>"+"before attempting to home" + "<br>"+ "(To avoid accidental nozzle crashes into the build platform)."+"</html>"), "wrap"); //Z axis lift
-		mainpanel.add(zAxisMMToLift,"gapleft 30, wrap");
-		mainpanel.add(makeButtonPanel());
+
+		mainpanel.add(makeConfigPanel(), "wrap"); //panel with Description, XYZ and Settings
+		mainpanel.add(makeButtonPanel(), "wrap"); //commit and cancel buttons
 		add(mainpanel); //return this panel...
 		//Update Zaxis mm to lift from eeprom settings...
 		
--- src/replicatorg/app/ui/controlpanel/Endstop3AxisPanel.java
+++ src/replicatorg/app/ui/controlpanel/Endstop3AxisPanel.java
@@ -16,6 +16,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.vecmath.Point3d;
@@ -30,6 +31,13 @@ public class Endstop3AxisPanel extends JPanel implements ActionListener
 	private static final long serialVersionUID = 1L;
 	protected MachineController machine;
 	protected Driver driver;
+	
+	public JToggleButton xPlusButton = createEndstopButton("X+", "Home X axis in positive direction");
+	public JToggleButton xMinusButton = createEndstopButton("X-", "Home X axis in negative direction");
+	public JToggleButton yPlusButton = createEndstopButton("Y+", "Home Y axis in positive direction");
+	public JToggleButton yMinusButton = createEndstopButton("Y-", "Home Y axis in negative direction");
+	public JToggleButton zPlusButton = createEndstopButton("Z+", "Home Z axis in positive direction");
+	public JToggleButton zMinusButton = createEndstopButton("Z-", "Home Z axis in negative direction");
 
 	/**
 	 * Create a jog-style button with the given name and tooltip.  By default, the
@@ -39,9 +47,9 @@ public class Endstop3AxisPanel extends JPanel implements ActionListener
 	 * @param tooltip the text to display when the mouse hovers over the button.
 	 * @return the generated button.
 	 */
-	protected JButton createEndstopButton(String text, String tooltip) {
+	protected JToggleButton createEndstopButton(String text, String tooltip) {
 		final int buttonSize = 60;
-		JButton b = new JButton(text);
+		JToggleButton b = new JToggleButton(text);
 		b.setToolTipText(tooltip);
 		b.setMaximumSize(new Dimension(buttonSize, buttonSize));
 		b.setPreferredSize(new Dimension(buttonSize, buttonSize));
@@ -59,8 +67,8 @@ public class Endstop3AxisPanel extends JPanel implements ActionListener
 	 * @param action the string representing the action.
 	 * @return the generated button.
 	 */
-	protected JButton createEndstopButton(String text, String tooltip, String action) {
-		JButton button = createEndstopButton(text,tooltip);
+	protected JToggleButton createEndstopButton(String text, String tooltip, String action) {
+		JToggleButton button = createEndstopButton(text,tooltip);
 		button.setActionCommand(action);
 		return button;
 	}
@@ -79,43 +87,23 @@ public class Endstop3AxisPanel extends JPanel implements ActionListener
 		this.driver = machine.getDriver();
 		setLayout(new MigLayout());
 		
-		// compile our regexes
-		//jogRate = 10.0;
-		//jogPattern = Pattern.compile("([.0-9]+)");
-
-		JButton xPlusButton = createEndstopButton("X+", "Home X axis in positive direction");
+		/*JButton xPlusButton = createEndstopButton("X+", "Home X axis in positive direction");
 		JButton xMinusButton = createEndstopButton("X-", "Home X axis in negative direction");
-		//JButton xCenterButton = createEndstopButton("<html><center>Center<br/>X", "Jog X axis to the origin","Center X");
 		JButton yPlusButton = createEndstopButton("Y+", "Home Y axis in positive direction");
 		JButton yMinusButton = createEndstopButton("Y-", "Home Y axis in negative direction");
-		//JButton yCenterButton = createEndstopButton("<html><center>Center<br/>Y", "Jog Y axis to the origin","Center Y");
 		JButton zPlusButton = createEndstopButton("Z+", "Home Z axis in positive direction");
 		JButton zMinusButton = createEndstopButton("Z-", "Home Z axis in negative direction");
-		//JButton zCenterButton = createEndstopButton("<html><center>Center<br/>Z", "Jog Z axis to the origin","Center Z");
-		//JButton zeroButton = createEndstopButton("<html><center>Set<br/>zero","Mark Current Position as Zero (0,0,0)","Zero");
-
+*/
 		JPanel xyzPanel = new JPanel(new MigLayout("","[]0[]","[]0[]"));
-        //xyzPanel.add(zCenterButton, "split 3,flowy,gap 0 0 0 0");
+
 		xyzPanel.add(xMinusButton, "cell 0 1, gap 0 0 0 0");
-        //xyzPanel.add(yCenterButton);
 		xyzPanel.add(yPlusButton, "split 2, flowy, gapbottom 54");
-		//xyzPanel.add(zeroButton,"gap 0 0 0 0");
 		xyzPanel.add(yMinusButton, "gap 0 0 0 0");
 		xyzPanel.add(xPlusButton,"cell 2 1, gapafter 10");
-       //xyzPanel.add(xCenterButton);
 		xyzPanel.add(zPlusButton, "split 2,flowy,gap 0 0 0 0");
 		xyzPanel.add(zMinusButton);
 
-		// create our position panel
-		//JPanel positionPanel = new JPanel(new MigLayout("flowy"));
-		// our label
-		//positionPanel.add(new JLabel("Jog Size"));
-		
-		// add it all to our jog panel
 		add(xyzPanel);
-		//add(positionPanel,"growx,wrap");
-		//add(feedratePanel,"growx,spanx");
-
 		// add jog panel border and stuff.
 		setBorder(BorderFactory.createTitledBorder("Homing Endstops"));
 	
@@ -124,131 +112,39 @@ public class Endstop3AxisPanel extends JPanel implements ActionListener
 
 	DecimalFormat positionFormatter = new DecimalFormat("###.#");
 
-	/*synchronized public void updateStatus() {
-		Point3d current = driver.getCurrentPosition();
-
-		xPosField.setText(positionFormatter.format(current.x));
-		yPosField.setText(positionFormatter.format(current.y));
-		zPosField.setText(positionFormatter.format(current.z));		
-	}*/
-
-	/*public void handleChangedTextField(JTextField source)
-	{
-		String name = source.getName();
-
-		if (source.getText().length() > 0) {
-			if (name.equals("xy-feedrate-value")) {
-				xyFeedrateSlider.setValue(Integer.parseInt(source.getText()));
-			} else if (name.equals("z-feedrate-value")) {
-				zFeedrateSlider.setValue(Integer.parseInt(source.getText()));
-			} else {
-				Base.logger.warning("Unhandled text field: "+name);
-			}
-		}
-	}*/
-
 	public void actionPerformed(ActionEvent e) {
-		//Point3d current = driver.getCurrentPosition();
-		//double xyFeedrate = xyFeedrateSlider.getValue();
-		//double zFeedrate = zFeedrateSlider.getValue();
 		String s = e.getActionCommand();
 
-		/*if(s.equals("handleTextfield"))
-		{
-			JTextField source = (JTextField) e.getSource();
-			handleChangedTextField(source);
-			source.selectAll();
-		}*/
-
+		
 		if (s.equals("X+")) {
-			//current.x += jogRate;
-
-			//driver.setFeedrate(xyFeedrate);
-			//driver.queuePoint(current);
+		//set x varibles
+			
+			xMinusButton.setSelected(false);
+			xPlusButton.setSelected(true);
 		} else if (s.equals("X-")) {
-			/*current.x -= jogRate;
-
-			driver.setFeedrate(xyFeedrate);
-			driver.queuePoint(current);*/
+			
+			xMinusButton.setSelected(true);
+			xPlusButton.setSelected(false);
 		} else if (s.equals("Y+")) {
-			/*current.y += jogRate;
-
-			driver.setFeedrate(xyFeedrate);
-			driver.queuePoint(current);*/
+			yMinusButton.setSelected(false);
+			yPlusButton.setSelected(true);
+		
 		} else if (s.equals("Y-")) {
-			/*current.y -= jogRate;
-
-			driver.setFeedrate(xyFeedrate);
-			driver.queuePoint(current);*/
+			yMinusButton.setSelected(true);
+			yPlusButton.setSelected(false);
+		
 		} else if (s.equals("Z+")) {
-			/*current.z += jogRate;
-
-			driver.setFeedrate(zFeedrate);
-			driver.queuePoint(current);*/
+			zMinusButton.setSelected(false);
+			zPlusButton.setSelected(true);
+		
 		} else if (s.equals("Z-")) {
-			/*current.z -= jogRate;
-
-			driver.setFeedrate(zFeedrate);
-			driver.queuePoint(current);*/
-		} else if (s.equals("Center X")) {
-			/*current.x = 0;
-
-			driver.setFeedrate(xyFeedrate);
-			driver.queuePoint(current);*/
-		} else if (s.equals("Center Y")) {
-			/*current.y = 0;
-
-			driver.setFeedrate(xyFeedrate);
-			driver.queuePoint(current);*/
-		} else if (s.equals("Center Z")) {
-			/*current.z = 0;
-
-			driver.setFeedrate(zFeedrate);
-			driver.queuePoint(current);*/
-		} else if (s.equals("Zero")) {
-			// "Zero" tells the machine to calibrate its
-			// current position as zero, not to move to its
-			// currently-set zero position.
-			//driver.setCurrentPosition(new Point3d());
-		}
-		// get our new jog rate
-		else if (s.equals("jog size")) {
-			/*JComboBox cb = (JComboBox) e.getSource();
-			String jogText = (String) cb.getSelectedItem();
-
-			// look for a decimal number
-			Matcher jogMatcher = jogPattern.matcher(jogText);
-			if (jogMatcher.find())
-				jogRate = Double.parseDouble(jogMatcher.group(1));
-
-			// TODO: save this back to our preferences file.
-
-			// System.out.println("jog rate: " + jogRate);*/
+			zMinusButton.setSelected(true);
+			zPlusButton.setSelected(false);
+		
 		} else {
+		
 			Base.logger.warning("Unknown Action Event: " + s);
 		
 	}
 	}
-	/*public void stateChanged(ChangeEvent e) {
-		JSlider source = (JSlider) e.getSource();
-		int feedrate = source.getValue();
-
-		if (source.getName().equals("xy-feedrate-slider")) {
-			xyFeedrateValue.setText(Integer.toString(feedrate));
-		} else if (source.getName().equals("z-feedrate-slider")) {
-			zFeedrateValue.setText(Integer.toString(feedrate));
-		}
-	}*/
-
-	
-	/* (non-Javadoc)
-	 * @see java.awt.event.FocusListener#focusGained(java.awt.event.FocusEvent)
-	 */
-	/*public void focusGained(FocusEvent e) {
-	}*/
-
-	/*public void focusLost(FocusEvent e) {
-		JTextField source = (JTextField) e.getSource();
-		//handleChangedTextField(source);
-	}*/
 }
